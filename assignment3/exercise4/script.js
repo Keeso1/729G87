205fa7c2ea57fe12104f529db9940d68832553c5
melkerhallstrom <melha133@student.liu.se>
2024-11-26 22:11:08 +0100

    assignment3,exercise4

---
+++ assignment3/exercise4/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    init();
+  });
+
+
+
+function init (){
+    const circles = document.querySelectorAll('.row .circle');
+    const squares = document.querySelectorAll('.row .square');
+    const rectangles = document.querySelectorAll('.row .rectangle');
+
+    circles.forEach((element) => {
+    element.addEventListener('click', ()=>{
+        if (element.classList.contains("green")){
+            element.classList.remove("green");
+        } else {
+        element.classList.add("green");
+     }})
+    })
+
+    squares.forEach((element) => {
+        element.addEventListener('click', () =>{
+            const currentHeight = parseFloat(getComputedStyle(element).height); 
+            const currentWidth = parseFloat(getComputedStyle(element).width); 
+            element.style.height = (currentHeight / 2) + "px"; 
+            element.style.width = (currentWidth / 2) + "px"; 
+        })
+    })
+    
+    rectangles.forEach((element) => {
+        element.addEventListener('click', () =>{
+            const currentPosition = parseFloat(getComputedStyle(element).marginBottom); 
+            element.style.marginBottom = (currentPosition - 10) + "px"; 
+        })
+    })
+}
