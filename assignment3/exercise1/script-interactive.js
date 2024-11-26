23f4b24fbe18beaeeb66ddf8bbe454823f1213c4
Isac Lindh <isac.lindh01@gmail.com>
2024-11-26 08:50:10 +0100

    A3 e2 e3 started

--- assignment3/exercise1/script-interactive.js
+++ assignment3/exercise1/script-interactive.js
@@ -1,12 +1,8 @@
 const blueSquare = document.getElementById('blueSquare');
 
-function mouseOver(Object){
-    Object.style.display = 'none';
-  }
-
 document.addEventListener('DOMContentLoaded', () => {
     blueSquare.addEventListener('mouseover',
         () => {
-          mouseOver(blueSquare);
+          blueSquare.style.display = 'none';
         },);
   });
