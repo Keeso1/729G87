abfe5994a4aa55c9911a2d00c3f7d752ee548c41
Isac Lindh <isac.lindh01@gmail.com>
2024-11-27 14:38:42 +0100

    exercise3 fix

--- assignment3/exercise3/script.js
+++ assignment3/exercise3/script.js
@@ -6,25 +6,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function init(){
     const styleInput = document.querySelector('#styleInput');
-    let styleOutput = document.querySelector('#styleOutput');
-    const styleSelect = document.querySelector("#styleSelect");
     const changeStyleBtn = document.querySelector(".changeStyle-btn");
+    let styleOutput = document.querySelector(".content div");
+    let styleSelect = document.querySelector('#styleSelect');
     const newParagraphBtn = document.querySelector(".newParagraph-btn");
-    const content = document.querySelector(".content")
+    const content = document.querySelector(".content");
 
     changeStyleBtn.addEventListener("click", () => {
-        const selectedFont = styleSelect.value;
-        styleOutput.style.fontFamily = selectedFont;
+        const output = styleSelect.value;
+        styleOutput.style.fontFamily = output;
     });
 
     styleInput.addEventListener('input', ()=> {
-        styleOutput.value = styleInput.value;
+        styleOutput.innerHTML = styleInput.value;
            }
     )
 
     newParagraphBtn.addEventListener('click',()=>{
-        let newPar = document.createElement("textarea")
-        content.appendChild(newPar)
+        let newPar = document.createElement("div");
+        content.appendChild(newPar);
         styleOutput = newPar;
     }
 
