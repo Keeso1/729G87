6d41c4faf37e5b9ee296955336e2c3f42355da2c
melkerhallstrom <melha133@student.liu.se>
2024-12-03 14:24:32 +0100

    assignment4E2

--- assignment4/exercise2/toggle-button.js
+++ assignment4/exercise2/toggle-button.js
@@ -5,9 +5,20 @@ class MyComponent extends HTMLElement {
       this.toggle_button_template = document.createElement("template");
       this.toggle_button_template.innerHTML =
       `
+    <style>
+    html {
+      box-sizing: border-box;
+    }
+    
+
+
+  </style>
+
       <div class="wrapper">
         <p>label text</p>
-        <div class="toggle" aria-pressed="false"></div>
+        <div class="toggle" aria-pressed="false">
+        <div class="box">
+        </div>
       </div>
 `
  
