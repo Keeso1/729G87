dfbbd4600b6a2013504477978c0b16e0eb352939
Isac Lindh <isac.lindh01@gmail.com>
2024-12-05 14:21:48 +0100

    accordion

--- assignment4/exercise3/accordion.js
+++ assignment4/exercise3/accordion.js
@@ -5,42 +5,73 @@ class MyComponent extends HTMLElement {
       this.accordion_component_template.innerHTML =
       `
       <style>
-    html {
-      box-sizing: border-box;
-    }
-   .accordion button::before {
-  content: "▶︎ ";
-}
+        html {
+          box-sizing: border-box;
+        }
+        .accordion button::before {
+          content: "▶︎ ";
+          animation: rotate 1s;
+          animation-direction: reverse;
+        }
 
-.accordion button[aria-expanded="true"]::before {
-  content: "▼︎ ";
-}
+        .accordion button[aria-expanded="true"]::before {
+          content: "▼︎ ";
+          animation: rotate 1s;
+        }
+        
+        @keyframes rotate {
+          from {
+            transform: rotate(0deg);
+          }
+          to {
+            transform: rotate(360deg);
+          }
+        }
 
 
-  </style>
-     <div class="accordion">
-  <ul>
-    <li>
-       <button aria-expanded="false">Accordion 1 title</button>
-       <div role="region" hidden=""><p>Content for Accordion 1</p></div>
-    </li>
-  </ul>
-</div>
-
+      </style>
+      <div class="accordion">
+        <ul>
+        </ul>
+      </div>
       `
       this.attachShadow({ mode: 'open' });
       this.shadowRoot.appendChild(this.accordion_component_template.content.cloneNode(true));
-
+      this.accordion = this.shadowRoot.querySelector('.accordion ul');
     }
     connectedCallback() {
-        let accordionHeadercollection = Array.from(this.getElementsByTagName('h2'));
-        console.log(accordionHeadercollection);
+      let HeaderCollection = Array.from(this.getElementsByTagName('h2'));
+      let contentCollection = Array.from(this.getElementsByTagName('div'));
 
-        accordionHeadercollection.forEach(element => {
-          this.createElement
-          this.shadowRoot.querySelector()
-          console.log(element.textContent); // Loop over and log each element's text
+      HeaderCollection.forEach((heading, index) => {
+        let listItem = document.createElement("li");
+        let text = document.createElement("div");
+        let button = document.createElement("button");
+
+        text.setAttribute("role", "region");
+        text.setAttribute("hidden", "");
+        button.setAttribute("aria-expanded", "false");
+
+        this.accordion.appendChild(listItem);
+        listItem.appendChild(button);
+        listItem.appendChild(text);
+
+        button.innerHTML = heading.textContent;
+        text.textContent = contentCollection[index].textContent;
+
+        button.addEventListener('click', () =>{
+          let expanded = button.getAttribute("aria-expanded") === "true";
+          console.log(expanded);
+          button.setAttribute("aria-expanded", !expanded);
+          text.hidden = !text.hidden;
+          if (expanded){
+
+          } else{
+            
+          }
+        })
       });
+      
       
         }
         
