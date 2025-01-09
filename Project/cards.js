7b178f5a55678763632e4d450a88b6dd1f7c812a
melkerhallstrom <melha133@student.liu.se>
2025-01-09 09:51:22 +0100

    slider done

--- Project/cards.js
+++ Project/cards.js
@@ -13,28 +13,28 @@ class MyComponent extends HTMLElement {
 
   .card {
     z-index: 1;
+    font-size:2px;
     position: relative;
-    min-height: 43.75rem;
+    min-height: 350em;
     display: grid;
     justify-content: center;
-    grid-template-columns: auto 26.25rem auto auto;
+    grid-template-columns: auto 210em auto auto;
     grid-template-rows: auto auto auto auto;
-    column-gap: 2rem;
+    column-gap: 16em;
     font-family: 'Rajdhani';
     overflow: hidden;
     background-image: url("images/rectangle-113-6.svg");
     background-repeat:no-repeat;
-    border-right: 8px solid rgba(94, 246, 255, 0.5);
-    width:0px;
-    margin-right: 1077px;
+    border-right: 4em solid rgba(94, 246, 255, 0.5);
+    width:0%;
+    margin-right: 100%;
     transition: 1.5s ease-in;
     animation:flicker 0.1s infinite;
 }
 
 .card.reveal{
-    width:1077px;
-    clip-path: inset(0); 
-    margin:0px;
+    width:538.5em;
+    clip-path:inset(0);
 }
 
   .cardheader{
@@ -42,11 +42,12 @@ class MyComponent extends HTMLElement {
     grid-column-start: 2;
     grid-row-end: 2;
     grid-column-end: 2;
-    font-size: 3rem;
+    font-size: 24em;
     font-weight:bold;
     text-align: left;
     color: rgba(94, 246, 255, 1);
     filter:drop-shadow(0 0 5px rgba(94, 246, 255, 0.7));
+  
   }
   
   
@@ -55,25 +56,22 @@ class MyComponent extends HTMLElement {
     grid-column-start: 2;
     grid-row-end: 3;
     grid-column-end: 2;
-    font-size: 1.5rem;
+    font-size: 12em;
     font-weight: bold;
     text-align: left;
     color: rgba(94, 246, 255, 1);
     filter:drop-shadow(0 0 5px rgba(94, 246, 255, 0.7));
   }
   
-  
-  
- 
   .cardimage {
     grid-row-start:3;
     grid-column-start: 3;
     grid-row-end: 3;
     grid-column-end: 3;
-    border: 1px solid rgba(94, 246, 255, 1);
-    border-radius: 1.25rem;
-    width: 23.125rem;
-    height: 21.875rem;
+    border: 0.5em solid rgba(94, 246, 255, 1);
+    border-radius: 10em;
+    width: 185em;
+    height: 175em;
     filter:drop-shadow(0 0 5px rgba(94, 246, 255, 1));
   }
   
@@ -144,6 +142,7 @@ class MyComponent extends HTMLElement {
     
       const slider = document.querySelector(".slider");
 
+
         if (slider) {
             let isScrolling;
 
@@ -162,8 +161,13 @@ class MyComponent extends HTMLElement {
             const resetSlider = () => {
               slider.classList.remove("scaled");
             }
+            slider.addEventListener('wheel', (evt) => {
+              evt.preventDefault();
+              slider.scrollLeft += evt.deltaY;
+              
+          });
 
-            slider.addEventListener("scroll", () => {
+            slider.addEventListener("scroll", () => { 
                 scaleSlider();
                 scaleCard();
                 
@@ -173,7 +177,7 @@ class MyComponent extends HTMLElement {
                 isScrolling = setTimeout(() => {
                     resetCard();
                     resetSlider();
-                }, 0);
+                }, 100);
             });
       }
     }
